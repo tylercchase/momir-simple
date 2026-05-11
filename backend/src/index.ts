5cc713cd834caaaf6df0251e89bd44520bacbfc1
tylercchase <tylercchase@gmail.com>
2026-05-10 23:07:29 -0400

    feat: init printer test script

---
+++ backend/src/index.ts
@@ -0,0 +1,3 @@
+import { testPrint } from "./print";
+
+testPrint();
---
+++ backend/src/print/index.ts
@@ -0,0 +1,87 @@
+const {
+    ThermalPrinter,
+    PrinterTypes,
+    CharacterSet,
+    BreakLine,
+} = require("node-thermal-printer");
+import { getDeviceList } from "usb";
+
+const PRINTER_PRODUCT_ID = 33054;
+
+const printToUSBPrinter = async (data) => {
+    const printer = getDeviceList().find((x) => {
+        return x.deviceDescriptor.idProduct === PRINTER_PRODUCT_ID;
+    });
+    if (!printer) {
+        throw new Error("Printer not found");
+    }
+
+    try {
+        printer.open();
+
+        const iface = printer.interfaces[0];
+        if (iface.isKernelDriverActive()) {
+            iface.detachKernelDriver();
+        }
+
+        iface.claim();
+
+        const outEndpoint = iface.endpoints.find((ep) => {
+            return ep.direction === "out";
+        });
+
+        if (!outEndpoint) {
+            throw new Error("No OUT endpoint found");
+        }
+
+        await new Promise<void>((resolve, reject) => {
+            outEndpoint.transfer(data, (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        });
+
+        iface.release(true, () => {
+            printer.close();
+        });
+
+        return "Data sent to printer successfully";
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const testPrint = async () => {
+    const printer = new ThermalPrinter({
+        type: PrinterTypes.EPSON,
+        width: 70,
+        interface: "dummy", // Dummy interface since we're not using execute
+        characterSet: CharacterSet.PC852_LATIN2,
+        removeSpecialCharacters: false,
+        lineCharacter: "=",
+        breakLine: BreakLine.WORD,
+    });
+
+    printer.alignCenter();
+    printer.println("Test Receipt");
+    printer.drawLine();
+    printer.println("Item 1 x 15 $15.00");
+    printer.println("Item 2 x 1 $75.00");
+    printer.drawLine();
+    printer.println("Total: $90.00");
+    printer.newLine();
+    printer.cut();
+
+    const buffer = printer.getBuffer();
+
+    console.log("Printing...");
+    try {
+        const result = await printToUSBPrinter(buffer);
+        console.log(result);
+    } catch (error) {
+        console.error(error.message);
+    }
+};
