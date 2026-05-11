ea06e6819b967f480a037dd955cc310f4f279583
tylercchase <tylercchase@gmail.com>
2026-05-11 00:25:14 -0400

    chore: move backend folder out of /src

--- src/backend/src/index.ts
+++
@@ -1,7 +0,0 @@
-
-// TOOD: possibly setup this in the root directory to pull from both backend and frontend
-const server = Bun.serve({
-    routes: {},
-});
-
-console.log(`Server running at ${server.url}`);
